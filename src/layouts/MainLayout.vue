b16a15367e9c4dd5d1f965c956d56598747ae053
Zero <zeroto521@gmail.com>
2021-03-07 10:45:18 +0800

    format sourse code & add lint for win os

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,45 +2,66 @@
   <q-layout view="hHh lpR fFf">
     <q-header elevated>
       <q-toolbar class="bg-grey-9">
-
         <q-avatar square size="md">
-          <img alt="FlowLauncher Logo" src="/images/logo.svg">
+          <img alt="FlowLauncher Logo" src="/images/logo.svg" />
         </q-avatar>
 
-        <q-toolbar-title>
-          FlowLauncher
-        </q-toolbar-title>
+        <q-toolbar-title> FlowLauncher </q-toolbar-title>
 
-        <q-btn size="sm" flat stretch icon="mdi-github" type="a" href="https://github.com/Flow-Launcher/Flow.Launcher">
+        <q-btn
+          size="sm"
+          flat
+          stretch
+          icon="mdi-github"
+          type="a"
+          href="https://github.com/Flow-Launcher/Flow.Launcher"
+        >
           &nbsp;Flow-Launcher/Flow.Launcher
         </q-btn>
       </q-toolbar>
+
       <q-toolbar class="bg-secondary shadow-8">
         <q-tabs align="left" inline-label>
-          <q-tab icon="mdi-home" label="Start" @click="scrollToElement('home')" />
-          <q-tab icon="mdi-cogs" label="Features"  @click="scrollToElement('features')" />
-          <q-tab icon="mdi-download" label="Download"  @click="scrollToElement('download')" />
+          <q-tab
+            icon="mdi-home"
+            label="Start"
+            @click="scrollToElement('home')"
+          />
+          <q-tab
+            icon="mdi-cogs"
+            label="Features"
+            @click="scrollToElement('features')"
+          />
+          <q-tab
+            icon="mdi-download"
+            label="Download"
+            @click="scrollToElement('download')"
+          />
         </q-tabs>
       </q-toolbar>
     </q-header>
+
     <q-page-container>
       <router-view />
     </q-page-container>
-      <q-toolbar class="row bg-grey-10 text-grey-4 shadow-up-4">
-        <q-space />
-          <div class="col-md-2 text-center col-xs-6">The FlowLauncher Team © 2020</div>
-        <q-space />
+
+    <q-toolbar class="row bg-grey-10 text-grey-4 shadow-up-4">
+      <q-space />
+      <div class="col-md-2 text-center col-xs-6">
+        The FlowLauncher Team © 2020
+      </div>
+      <q-space />
     </q-toolbar>
   </q-layout>
 </template>
 
 <script>
-import { scroll } from 'quasar';
+import { scroll } from "quasar";
 
 const { getScrollTarget, setScrollPosition } = scroll;
 
 export default {
-  name: 'MainLayout',
+  name: "MainLayout",
   methods: {
     scrollToElement(el) {
       const element = document.getElementById(el);
